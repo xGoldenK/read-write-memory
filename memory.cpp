7085a71186fcea590c2481cd338253eea068c8eb
Nicola Picarella <rialhaxor@gmail.com>
2023-05-22 00:21:13 +0200

    added debug privilege modified some functions

--- memory.cpp
+++ memory.cpp
@@ -1,8 +1,7 @@
 #include "memory.hpp"
-#include <iostream>
 #include <tlhelp32.h>
 
-int memory::get_proc_id(const char* process_name) {
+int memory::get_proc_id(const std::wstring& process_name) {
     int pid = 0;
     BOOL hResult;
     HANDLE hSnapshot;
@@ -24,12 +23,19 @@ int memory::get_proc_id(const char* process_name) {
     // retrieve information about the processes
     // and exit if unsuccessful
     while(hResult) {
+        // use wstring to compare
+        std::wstring temp(pe.szExeFile);
         // if we find the process: return process ID
-        if(strcmp(process_name, (const char*)pe.szExeFile) == 0) {
+        if(process_name.compare(temp) == 0) {
             pid = pe.th32ProcessID;
             break;
         }
         hResult = Process32Next(hSnapshot, &pe);
+    }
+
+    if(pid == 0) {
+        std::cout << "Could not find process \"" << process_name.c_str() << "\"." << std::endl;
+        std::cout << "Last error: " << GetLastError() << std::endl;
     }
 
     // closes an open handle (CreateToolhelp32Snapshot)
@@ -38,7 +44,13 @@ int memory::get_proc_id(const char* process_name) {
 }
 
 HANDLE memory::get_process_handle(int process_id) {
-    return OpenProcess(PROCESS_ALL_ACCESS, FALSE, process_id);
+    HANDLE h = OpenProcess(PROCESS_CREATE_PROCESS, FALSE, process_id);
+    if(!h) {
+        std::cout << "Could not get handle for pid: " << process_id << "." << std::endl;
+        std::cout << "Last error: " << GetLastError() << std::endl;
+    }
+
+    return h;
 }
 
 BOOL memory::write_to_process(HANDLE process, LPVOID address, LPCVOID buffer) {
@@ -61,4 +73,31 @@ BOOL memory::read_from_process(HANDLE process, LPVOID address, LPVOID* out_buffe
         std::cout << "ReadProcessMemory read 0 bytes!" << std::endl;
 
     return result;
+}
+
+BOOL memory::set_debug_privilege(BOOL b) {
+    LUID privilegeId;
+    HANDLE hToken;
+
+    if(!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+        std::cout << "Could not open process token for current process." << std::endl;
+        return FALSE;
+    }
+
+    if(!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &privilegeId)) {
+        std::cout << "Could not lookup SE_DEBUG privilege" << std::endl;
+        return FALSE;
+    }
+
+    TOKEN_PRIVILEGES tokenPriv;
+    tokenPriv.PrivilegeCount = 1;
+    tokenPriv.Privileges[0].Luid = privilegeId;
+    tokenPriv.Privileges[0].Attributes = b ? SE_PRIVILEGE_ENABLED : 0;
+
+    if(!AdjustTokenPrivileges(hToken, FALSE, &tokenPriv, sizeof(TOKEN_PRIVILEGES), NULL, NULL)) {
+        std::cout << "Could not adjust privilege for token." << std::endl;
+        return FALSE;
+    }
+
+    return TRUE;
 }
--- memory.hpp
+++ memory.hpp
@@ -1,10 +1,11 @@
 #pragma once
-#include <Windows.h>
+#include <iostream>
+#include <windows.h>
 
 namespace memory {
-	int get_proc_id(const char* process_name);
+	int get_proc_id(const std::wstring& process_name);
 	HANDLE get_process_handle(int process_id);
 	BOOL write_to_process(HANDLE process, LPVOID address, LPCVOID buffer);
 	BOOL read_from_process(HANDLE process, LPVOID address, LPVOID* out_buffer);
-
+	BOOL set_debug_privilege(BOOL b);
 }
